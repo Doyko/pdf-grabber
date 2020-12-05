24219ad564e8567548e510aec5b83a62519e22bb
Doyko <benjamin.bihanic@hotmail.fr>
2020-12-05 23:51:49 +0100

    pdf download

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,48 @@
 use std::collections::HashSet;
-use std::io::Read;
 
-use reqwest;
-use reqwest::Url;
+use std::io::{self, Read};
+use std::fs::File;
+
+use reqwest::{self, Url};
 
 use regex::Regex;
 
+fn download_pdf(url: &str)
+{
+    println!("PDF found : {}", url);
+    let mut res = reqwest::blocking::get(url).unwrap();
+    let filename = url.split('/').next_back().unwrap();
+    let mut out = File::create(format!("pdf/{}", filename)).unwrap();
+
+    io::copy(&mut res, &mut out).unwrap();
+}
+
 fn normalize_url(url: &str, origin: &str) -> String {
+
     let normalized_url: &str;
+    
     if let Some(pos) = url.find('#') {
         normalized_url = &url[..pos];
     } else {
         normalized_url = url;
     }
 
-    if normalized_url.starts_with("/")
-    {
+    if normalized_url.starts_with("/") {
         return format!("{}{}", origin, normalized_url);
     }
 
     normalized_url.to_string()
 }
 
-fn fetch_url(client: &reqwest::blocking::Client, url: &str) -> String {
+fn fetch_url(client: &reqwest::blocking::Client, url: &str) -> Option<String> {
     let mut res = client.get(url).send().unwrap();
     println!("Status for {}: {}", url, res.status());
 
     let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
-    body
+    match res.read_to_string(&mut body){
+        Ok(_) => Some(body),
+        Err(_) => None
+    }
 }
 
 fn get_link_from_url(html: &str, origin_url: &str) -> HashSet<String> {
@@ -44,8 +58,8 @@ fn check_url(url: &str, origin: &str) -> Option<String> {
     let normalized_url = normalize_url(url, origin);
 
     if url.ends_with(".pdf") {
-        //todo Download PDF
-        return None;
+        download_pdf(&normalized_url);
+        return Some(normalized_url);
     }
 
     let parsed_url = Url::parse(&normalized_url);
@@ -66,9 +80,10 @@ fn check_url(url: &str, origin: &str) -> Option<String> {
 
 fn main() {
     let client = reqwest::blocking::Client::new();
-    let origin_url = "http://doyko.xyz";
+    // todo json as input
+    let origin_url = "https://rubytox.fr";
     // todo format with / to avoid double check
-    let body = fetch_url(&client, origin_url);
+    let body = fetch_url(&client, origin_url).unwrap();
 
     let mut visited_url = HashSet::new();
     visited_url.insert(origin_url.to_string());
@@ -81,10 +96,8 @@ fn main() {
     while !new_url.is_empty() {
         let found_urls: HashSet<String> = new_url
             .iter()
-            .map(|url| {
-                let body = fetch_url(&client, url);
-                get_link_from_url(&body, origin_url)
-            })
+            .filter_map(|url| fetch_url(&client, url))
+            .map(|html| get_link_from_url(&html, origin_url))
             .fold(HashSet::new(), |mut acc, x| {
                 acc.extend(x);
                 acc
